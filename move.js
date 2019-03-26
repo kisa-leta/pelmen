859d3677fa1221fbca08283612150ffbccd22d22
Виолетта Мацюшевская <leta@MacBook-Pro-Violetta.local>
2019-03-26 14:06:03 +0300

    kos

--- move.js
+++ move.js
@@ -20,25 +20,25 @@ function dblclick() {
     function leftArrowPressed() {
     var element = document.getElementById("zvizda");
     var currentX = parseInt(element.style, 10);
-    element.style.left = `${currentX - 30 < 0 ? currentX : currentX - 30}px`;
+    element.style.left = `${currentX - 230 < 0 ? currentX : currentX - 30}px`;
     }
 
     function rightArrowPressed() {
         var element = document.getElementById("zvizda");
         var currentX = parseInt(element.style.left, 10);
-        element.style.left = `${currentX + 30 > maxWidth ? currentX : currentX + 30}px`;
+        element.style.left = `${currentX + 230 > maxWidth ? currentX : currentX + 30}px`;
     }
     
     function upArrowPressed() {
         var element = document.getElementById("zvizda");
         var currentY = parseInt(element.style.top, 10);
-        element.style.top = `${currentY - 30 < minHeight ? currentY : currentY - 30}px`;
+        element.style.top = `${currentY - 230 < minHeight ? currentY : currentY - 30}px`;
     }
     
     function downArrowPressed() {
         var element = document.getElementById("zvizda");
         var currentY = parseInt(element.style.top, 10);
-        element.style.top = `${currentY + 30 > maxHeight ? currentY : currentY + 30}px`;
+        element.style.top = `${currentY + 230 > maxHeight ? currentY : currentY + 30}px`;
     }
 
     function moveSelection(evt) {
