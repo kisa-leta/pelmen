0af64471f61f3b417c5e3f91e6d285a51915a178
Виолетта Мацюшевская <leta@MacBook-Pro-Violetta.local>
2019-05-27 12:14:41 +0300

    kisa

--- move.js
+++ move.js
@@ -17,25 +17,31 @@ function dblclick() {
     element.src = images[currentId];
 }
 
+ 
+
     function leftArrowPressed() {
+        leftExt.preventDefault()
         var element = document.getElementById("zvizda");
         var currentX = parseInt(element.style.left, 10);
         element.style.left = `${currentX - 30 < 0 ? currentX : currentX - 30}px`;
     }
 
     function rightArrowPressed() {
+        rightExt.preventDefault()
         var element = document.getElementById("zvizda");
         var currentX = parseInt(element.style.left, 10);
         element.style.left = `${currentX + 230 > maxWidth ? currentX : currentX + 30}px`;
     }
     
     function upArrowPressed() {
+        upExt.preventDefault()
         var element = document.getElementById("zvizda");
         var currentY = parseInt(element.style.top, 10);
         element.style.top = `${currentY + 130 < minHeight ? currentY : currentY - 30}px`;
     }
     
     function downArrowPressed() {
+        downExt.preventDefault()
         var element = document.getElementById("zvizda");
         var currentY = parseInt(element.style.top, 10);
         element.style.top = `${currentY + 230 > maxHeight ? currentY : currentY + 30}px`;
